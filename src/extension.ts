3cd8fef973ae5e1e765978e2d153a6e2bb907754
Martin Zamba <martin.zamba@gmail.com>
2023-02-19 07:40:01 +0100

    feat: first impl. of 'Run Selection' command

--- src/extension.ts
+++ src/extension.ts
@@ -21,49 +21,92 @@ declare global {
 	var outputWindow: vscode.OutputChannel;
 }
 
+const EXT_ID = "run-section";
+
 // ======================================== //
 //                 Functions                //
 // ======================================== //
-function log(msg: string) {
-	console.log(msg);
+function wrap_msg(msg: string): string {
+	return `${EXT_ID}: ${msg}`;
 }
 
-function err(msg: string) {
-	console.error(msg);
-}
-
-function out(msg: string) {
+function out(msg: string): void {
+	if (!global.outputWindow) {
+		global.outputWindow = vscode.window.createOutputChannel(EXT_ID);
+	}
 	global.outputWindow.appendLine(msg);
 }
 
+function log(msg: string): void {
+	msg = wrap_msg(msg);
+	console.log();
+	out(msg);
+}
+
+function err(msg: string): void {
+	msg = wrap_msg(msg);
+	console.error(msg);
+	out(msg);
+}
 
 // ======================================== //
 //                    Main                  //
 // ======================================== //
 // Called when your extension is activated 
 export function activate(context: vscode.ExtensionContext) {
-	log('run-section extension active');
-
-	global.outputWindow = vscode.window.createOutputChannel("hello");
-
-	out("run-section extension activated");
+	out("extension activated");
 
 	// Will say hello to you
-	let cmd_sayHello = vscode.commands.registerCommand('run-section.sayHello', () => {
+	let cmd_sayHello = vscode.commands.registerCommand(`${EXT_ID}.sayHello`, () => {
+		log('sayHello command invoked');
 		vscode.window.showInformationMessage('Hello !!! World from Run Section!');
 	});
 
 	// Show extension's output window
-	let cmd_showOutputWindow = vscode.commands.registerCommand('run-section.showOutputWindow', () => {
+	let cmd_showOutputWindow = vscode.commands.registerCommand(`${EXT_ID}.showOutputWindow`, () => {
+		log('showOutputWindow command invoked');
 		global.outputWindow.show();
 	});
 
 	// Will show current time
-	let cmd_showTime = vscode.commands.registerCommand('run-section.showTime', () => {
+	let cmd_showTime = vscode.commands.registerCommand(`${EXT_ID}.showTime`, () => {
+		log('showTime command invoked');
 		vscode.window.showWarningMessage('The time is ' + new Date().toLocaleTimeString());
 	});
 
-	for (let cmd of [cmd_sayHello, cmd_showOutputWindow, cmd_showTime]) {
+	// Will run selected text or current line if no text is selected
+	let cmd_runSelection = vscode.commands.registerCommand(`${EXT_ID}.runSelection`, () => {
+		log('runSelection command invoked');
+
+		// get text on current line
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		} else {
+			var selection = editor.selection;
+			var text = editor.document.getText(selection);
+			if (text.length === 0) {
+				// get current line and strip leading and trailing whitespace
+				let line = editor.document.lineAt(selection.active.line);
+				text = line.text.trim();
+			}
+			out(text);
+		}
+		log('Obtained text: ' + text);
+
+		// get current terminal or create new one
+		let terminal = vscode.window.activeTerminal;
+		if (!terminal) {
+			log('No interactive terminal available');
+			vscode.window.showErrorMessage('Create at least one terminal first');
+			return;
+		}
+		log('Running selection in terminal: ' + terminal.name);
+
+		terminal.sendText(text);
+	});
+
+	for (let cmd of [cmd_sayHello, cmd_showOutputWindow, cmd_showTime, cmd_runSelection]) {
 		context.subscriptions.push(cmd);
 	}
 }
